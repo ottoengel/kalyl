9c89836c7cdec7ccd48f1a845d83061f7979302b
Otto <ottoengelhardt18@gmail.com>
2024-09-29 20:28:17 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button"
+
 const Home = () => {
   return (
     <div>
       <h1 className="text-red-500">Home Page</h1>
+      <Button variant="destructive">Teste</Button>
     </div>
   )
 }
