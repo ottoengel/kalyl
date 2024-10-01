5333d66ff27c606d7ea173398aaa55d71a85c006
Otto <ottoengelhardt18@gmail.com>
2024-09-30 22:26:05 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import Image from "next/image"
 import { Input } from "./_components/ui/input"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -10,15 +13,17 @@ const Home = () => {
       {/* HEADER */}
       <Header />
       <div className="p-5">
+        {/* TEXTO */}
         <h2 className="text-xl font-bold">Olá, User</h2>
         <p>Segunda Feira, 00 de setembro.</p>
-
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button className="bg-[#F6484B]" variant="outline">
             <SearchIcon />
           </Button>
         </div>
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com o Kalyls"
@@ -27,6 +32,34 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge
+                className="w-fit bg-[#73f086a1] text-[#62FFD3]"
+                variant="outline"
+              >
+                Confirmado
+              </Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="/user2.jpg" />
+                </Avatar>
+                <p className="text-sm">Lucas</p>
+              </div>
+            </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Outubro</p>
+              <p className="text-2xl">01</p>
+              <p className="text-sm">10:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
