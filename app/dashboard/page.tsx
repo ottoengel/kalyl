9ddebbef336d6ce471d2bfc94126dcd6ca8e4afe
Otto <ottoengelhardt18@gmail.com>
2024-10-31 16:33:09 -0300

    feat: add initial admin dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../_components/header"
+
+const Dashboard = async () => {
+  return (
+    <div>
+      <Header />
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
