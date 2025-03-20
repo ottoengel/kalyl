7f62bf8064b814103129cda69de8f40b2606d5dc
Otto <ottoengelhardt18@gmail.com>
2025-03-19 22:02:29 -0300

    feat: Booking

--- app/plan-dashboard/page.tsx
+++ app/plan-dashboard/page.tsx
@@ -1,17 +1,23 @@
-'use client'
+"use client"
+import { Barber, BarberServices } from "@prisma/client"
 import Header from "../_components/header"
 import Booking from "./section/booking-section"
 import Hero from "./section/hero"
-import {ParallaxProvider} from "react-scroll-parallax"
+import { ParallaxProvider } from "react-scroll-parallax"
 
-const planDashboard = () => {
+interface planDashboardProps {
+  service: Pick<BarberServices, "name" | "price" | "id">
+  barber: Pick<Barber, "name" | "id">
+}
+
+const planDashboard = ({ service, barber }: planDashboardProps) => {
   return (
     <>
       <Header />
       <ParallaxProvider>
-      <Hero />
+        <Hero />
       </ParallaxProvider>
-      <Booking />
+      <Booking service={service} barber={barber} />
     </>
   )
 }
