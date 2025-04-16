30b766370e77e8a87b2f9ae21460225927495cf7
SamuelGuibor <guiborsamuel@gmail.com>
2025-04-16 14:05:08 -0300

    horario alterado

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -76,11 +76,11 @@ const getTimeList = ({
 
     if (dayOfWeek === 2 || dayOfWeek === 4) {
       availableTimes = availableTimes.filter((time) => Number(time.split(":")[0]) < 13);
-    } else if (dayOfWeek === 5) {
-      availableTimes = availableTimes.filter((time) => {
-        const hour = Number(time.split(":")[0]);
-        return hour >= 13;
-      });
+    // } else if (dayOfWeek === 5) {
+    //   availableTimes = availableTimes.filter((time) => {
+    //     const hour = Number(time.split(":")[0]);
+    //     return hour >= 13;
+    //   });
     } 
   }
 
