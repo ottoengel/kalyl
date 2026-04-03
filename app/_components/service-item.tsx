3ec1b9680ab35021b8badcaee7b51e2bfb3c529f
SamuelGuibor <guiborsamuel@gmail.com>
2026-04-02 22:11:07 -0300

    dale

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -89,7 +89,7 @@ const getTimeList = ({
   // Aplicar condições de forma mutuamente exclusiva
   if (normalizedBarberId === otherBarber) {
     // availableTimes = ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00"];
-    availableTimes = ["09:00",
+    availableTimes = [
       "10:00",
       "11:00",
       "12:00",
@@ -100,7 +100,7 @@ const getTimeList = ({
       "17:00",
       "18:00",
       "19:00",
-      "20:00"];
+    ];
   } else if (normalizedBarberId === specialBarberId) {
     availableTimes.unshift("08:00", "09:00");
     // if (dayOfWeek === 6) {
